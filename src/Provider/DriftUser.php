edae024cb00080f36f6a44610f24abbb18f9ca06
Sumaer Jolly <sumaerjolly@gmail.com>
2020-11-24 12:43:15 +0530

    add token and drift user

---
+++ src/Provider/DriftUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SumaerJolly\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Tool\ArrayAccessorTrait;
+
+class DriftUser implements ResourceOwnerInterface
+{
+  use ArrayAccessorTrait;
+
+  /**
+   * @var array
+   */
+  protected $response;
+
+  /**
+   * @param array $response
+   */
+  public function __construct(array $response)
+  {
+    $this->response = $response;
+  }
+
+  public function getId()
+  {
+    return $this->getValueByKey($this->response, 'data.0.id');
+  }
+
+  public function getName()
+  {
+    return $this->getValueByKey($this->response, 'data.0.name');
+  }
+
+  public function toArray()
+  {
+    return $this->response;
+  }
+}
